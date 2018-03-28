6c0a271ccfb6b51ef32add574857f1e06da95bff
unlimitedpractice <unlimited.guerrilla.works@gmail.com>
2018-03-28 14:46:45 +0900

    コマンドライン引数を追加メッセージとして表示できるように修正。 JavaDocを考慮したコメントを追加。また出力したJavaDocを追加。

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-
+/**
+ * メインクラス。
+ * エントリー(アプリ実行時、はじめに実行されるメソッド)を内包するクラス
+ * @author unlimitedpractice
+ */
 public class Main {
-
-	// メイン
+	/**
+	 * メインメソッド。
+	 * アプリ実行時、エントリーとなるメソッド(はじめに実行されるメソッド)
+	 * @param args String[] コマンドライン引数(複数可)
+	 */
 	public static void main(String[] args) {
-		System.out.print("Hello, Java");
-	}
+		// 表示メッセージ
+		String displayMessage = "Hello, Java";
 
+		// コマンドライン引数の文字列を追加メッセージとして連結する
+		String externalMessage = "";
+		for (String arg: args) {
+			externalMessage += " " + arg;
+		}
+
+		// コンソールにメッセージ表示
+		System.out.print(displayMessage + externalMessage);
+	}
 }
